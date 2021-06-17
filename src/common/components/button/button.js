3cf2252bdd3273471b38e8d5abb0ec0b64f1e369
rzounlom <rzounlom@gmail.com>
2021-06-17 09:18:21 -0400

    update login button

--- src/common/components/button/button.js
+++ src/common/components/button/button.js
@@ -3,8 +3,12 @@ import './buttons.scss';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const Button = ({ text }) => {
-  return <button className='general-button'>{text}</button>;
+const Button = ({ text, submit }) => {
+  return (
+    <button className='general-button' onClick={submit}>
+      {text}
+    </button>
+  );
 };
 
 Button.propTypes = {
--- src/login/components/loginForm.js
+++ src/login/components/loginForm.js
@@ -3,21 +3,41 @@ import './loginForm.scss';
 import React, { useState } from 'react';
 
 import Button from '../../common/components/button/button';
+import { withRouter } from 'react-router-dom';
 
-const LoginForm = () => {
+const LoginForm = ({ history }) => {
   const [loginCreds, setLoginCreds] = useState({ username: '', password: '' });
+  const onInputChange = (event) => {
+    const { name, value } = event.target;
+    setLoginCreds({ ...loginCreds, [name]: value });
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(history);
+    history.push('/');
+  };
 
   return (
     <div className='login-form-container'>
       <div className='login-form__inputs'>
         <h1>Scribe Afrique</h1>
         <h2>Admin Login</h2>
-        <input name='username' type='text' placeholder='username' />
-        <input nname='password' type='password' placeholder='password' />
-        <Button text='Login' />
+        <input
+          name='username'
+          type='text'
+          placeholder='username'
+          onChange={onInputChange}
+        />
+        <input
+          name='password'
+          type='password'
+          placeholder='password'
+          onChange={onInputChange}
+        />
+        <Button text='Login' submit={onSubmit} />
       </div>
     </div>
   );
 };
 
-export default LoginForm;
+export default withRouter(LoginForm);
