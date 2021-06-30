094b3ff77d576c18d77db69515e942a770731e7f
rzounlom <rzounlom@gmail.com>
2021-06-30 07:59:12 -0400

    add headers to apollo client requests

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -1,6 +1,24 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: process.env.REACT_APP_DEV_APOLLO_CLIENT_URI,
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 
 export const client = new ApolloClient({
-  uri: process.env.REACT_APP_DEV_APOLLO_CLIENT_URI,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
